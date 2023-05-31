41f0068b441ec265ca4056be82ab41fc76c89cef
Time2bImmortal <129998875+Time2bImmortal@users.noreply.github.com>
2023-05-31 14:42:31 +0300

    Starting the project

--- Features_extraction.py
+++ Features_extraction.py
@@ -1,5 +1,31 @@
 import os
 
+def plot_sound(filename):
+    # Load audio file
+    print(os.path.exists(filename))
+    # Load a .wav file
+    sr, audio = wavfile.read(filename)
+
+    # If stereo, convert to mono by averaging the two channels
+    if len(audio.shape) > 1:
+        audio = audio.mean(axis=1)
+
+    # Plot waveform
+    plt.figure(figsize=(12, 4))
+    plt.plot(audio)
+    plt.title('Waveform')
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Amplitude')
+    plt.show()
+
+    # Plot spectrogram
+    plt.figure(figsize=(12, 4))
+    D = librosa.amplitude_to_db(np.abs(librosa.stft(audio)), ref=np.max)
+    librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='log')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Spectrogram')
+    plt.show()
+
 
 def list_files_in_directory(directory_path):
     try:
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 from tkinter import filedialog
+import audioread
 import librosa
 import librosa.display
-import scipy as sp
-import IPython.display as ipd
 import matplotlib.pyplot as plt
+import os
 import numpy as np
+from scipy.io import wavfile
+
+
 def choose_folder():
     root = tk.Tk()
     # Hide the main window
@@ -15,6 +18,15 @@ def choose_folder():
     return folder_path
 
 
+def prepare_datasets(test_size, validation_size):
+    pass
+
+
 if __name__ == "__main__":
-    chosen_folder = choose_folder()
-    print(f'Chosen folder: {chosen_folder}')
+
+
+    #  create, train, validations sets
+    # build the cnn net
+    # train the cnn
+    # Evaluate the CNN on the test set
+    # Make predictions on a sample
